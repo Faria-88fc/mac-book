da015e3d4f7d8b4232e826d2caea2a397740731b
faria88fc <faria-2019614566@psy.du.ac.bd>
2021-08-21 15:49:44 +0600

    comment added

--- mac.js
+++ mac.js
@@ -1,6 +1,6 @@
 const extraMemoryCostt = document.getElementById('memory-8GB-btn').addEventListener('click', function(){
     productsCost('extra-memory-cost', 0);
-    updateTotal ()
+    updateTotal ();
 })
 const extraMemoryCost = document.getElementById('memory-16GB-btn').addEventListener('click', function(){
     productsCost("extra-memory-cost", 180);
@@ -11,26 +11,26 @@ const extraMemoryCost = document.getElementById('memory-16GB-btn').addEventListe
 
 const extraStorageCostt = document.getElementById('storage-256-ssd').addEventListener('click', function(){
     productsCost("extra-ssd-cost",0);
-    updateTotal ()
+    updateTotal ();
 })
 const storage = document.getElementById('storage-512-ssd').addEventListener('click', function(){
     productsCost("extra-ssd-cost",100);
-    updateTotal ()
+    updateTotal ();
 })
 const strCost = document.getElementById('storage-1tb').addEventListener('click', function(){
     productsCost("extra-ssd-cost",180,);
     console.log(productsCost);
-    updateTotal ()
+    updateTotal ();
 })
 
 
 const delivery = document.getElementById('free-delivery').addEventListener('click', function(){
     productsCost("delivery-charge",0);
-    updateTotal ()
+    updateTotal ();
 })
 const deliveryy = document.getElementById('delivery-withcharge').addEventListener('click', function(){
     productsCost("delivery-charge",20);
-    updateTotal ()
+    updateTotal ();
 })
 
 
@@ -42,18 +42,14 @@ document.getElementById('apply-btn').addEventListener('click',function(){
 
     if(promoInput == 'stevekaku'){
         const discountPrice = (subtotal-(subtotal*20)/100);
-        const grandTotal = document.getElementById('grand-total').innerText=discountPrice;
+        document.getElementById('grand-total').innerText=discountPrice;
     }
-
 })
-
-
 
 // extra cost setting function 
 function productsCost (cost, xtraCost){
-    const extraProductsCost = document.getElementById(cost);
-    extraProductsCost.innerText = xtraCost; 
-    }  
+    document.getElementById(cost).innerText =xtraCost; 
+}  
 
 // extra cost total funtion
 function updateTotal (){
